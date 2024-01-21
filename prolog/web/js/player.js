84b1f974379f4794be46485980445b33776f996b
Anne Ogborn <annie66us@yahoo.com>
2024-01-21 11:47:34 +0100

    brought art over from chr branch

---
+++ prolog/web/js/player.js
@@ -0,0 +1,7 @@
+document.addEventListener("keydown", keyDownTextField, false);
+
+function keyDownTextField(e) {
+var keyCode = e.keyCode.toString();
+window.location.href = "/?key=" + keyCode;
+}
+
